dc86d1bcc031a62e1820a9d31b795d05a8a443f2
Nikolas K <NCK@IIGHs-MBP-2.lan>
2022-02-11 21:56:04 -0800

    initial commit

---
+++ SignUpScreen/SignUpScreen/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  SignUpScreen
+//
+//  Created by Nikolas K. on 2/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var labelReg: UILabel!
+    @IBOutlet var emailField: UITextField!
+    @IBOutlet var passField: UITextField!
+    @IBOutlet var buttonNext: UIButton!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // MARK: Register Label
+        labelReg.text = "Register"
+        labelReg.font = UIFont(name: "Verdana", size: 30)
+        labelReg.textColor = .black
+        
+      
+        
+        // MARK: Email Text Field
+        emailField.placeholder = "jane@example.com"
+        emailField.font = UIFont(name: "Roboto", size: 15)
+        emailField.layer.borderColor = UIColor.black.cgColor
+        emailField.layer.borderWidth = 2.0
+        
+        // MARK: Password Text Field
+        passField.placeholder = "password"
+        passField.isSecureTextEntry = true
+        passField.font = UIFont(name: "Roboto", size: 15)
+        passField.layer.borderColor = UIColor.black.cgColor
+        passField.layer.borderWidth = 2.0
+        
+        // MARK: Next Button
+        buttonNext.setTitle("NEXT", for: .normal)
+        buttonNext.setTitleColor(.white, for: .normal)
+        buttonNext.backgroundColor = .black
+        buttonNext.titleLabel?.font = UIFont(name: "Roboto", size: 13)
+        // set font roboto and text size 13px
+    }
+
+
+}
+
